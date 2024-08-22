8d2a2705cc3b5cd100adbac39e905ea636b3f4c2
Wanyi Chen <wchen@corevitas.com>
2024-08-22 16:00:26 -0400

    add practice code script

---
+++ practice.R
@@ -0,0 +1,7 @@
+# practice: read datacut
+library(haven)
+
+pso_path <- paste0("../../../Users/", 
+                   Sys.getenv("USERNAME"), 
+                   "/Corrona LLC/Biostat Data Files - PsO/monthly/2023/2023-04-10")
+
