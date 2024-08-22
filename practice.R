f792ac63bccb35c9d60e47db6c844680a9a5d06e
Wanyi Chen <wchen@corevitas.com>
2024-08-22 16:04:14 -0400

    Delete practice.R

--- practice.R
+++
@@ -1,7 +0,0 @@
-# practice: read datacut
-library(haven)
-
-pso_path <- paste0("../../../Users/", 
-                   Sys.getenv("USERNAME"), 
-                   "/Corrona LLC/Biostat Data Files - PsO/monthly/2023/2023-04-10")
-
